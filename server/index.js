463e3a1b0e8b87fff2a14068103c8ec9fa87d145
Justin <justin.zeis@gmail.com>
2021-12-12 23:28:04 -0500

    adding port env variable back

--- server/index.js
+++ server/index.js
@@ -27,8 +27,7 @@ app.use('/league', leagueRouter);
 app.use('/team', teamRouter);
 
 const CONNECTION_URL = 'mongodb+srv://jcz:Hova8242$@cluster0.2lvfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-// const PORT = process.env.PORT|| 5000;
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
